9576453d828bee26c28832f7d97fa85d598382a1
Atlas Home <alhefnawimoh@hotmail.com>
2025-01-15 14:48:57 +0200

    create Advanced_flutter_arabic_mina

---
+++ lib/app/app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+   // default constructor
+
+  // named constructor
+MyApp._internal();
+
+int appState =0;
+
+static final MyApp _instance = MyApp._internal();  // single instance
+
+factory MyApp() => _instance;  // factory
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
+
+
