5d0d1535a5e6619fb936964199bd70116a7bf8c5
Atlas Home <alhefnawimoh@hotmail.com>
2025-01-16 13:57:11 +0200

    part 8 commit - Adding App Bar theme in theme manager

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,34 +1,40 @@
 import 'package:advanced_flutter/presentation/color_manager.dart';
+import 'package:advanced_flutter/presentation/font_manager.dart';
+import 'package:advanced_flutter/presentation/styles_manager.dart';
 import 'package:advanced_flutter/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
-ThemeData getApplicationTheme(){
+ThemeData getApplicationTheme() {
   return ThemeData(
     // Main Colors
     primaryColor: ColorManager.primary,
     primaryColorLight: ColorManager.lightPrimary,
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager.grey1,
+    splashColor: ColorManager.lightPrimary, // Ripple Effect Color
 
     // cardView Theme
-    cardTheme:CardTheme(
+
+    cardTheme: CardTheme(
       color: ColorManager.white,
       shadowColor: ColorManager.grey,
       elevation: AppSize.s4,
-    )
-
+    ),
 
     //app bar theme
 
+    appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManager.primary,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.lightPrimary,
+        titleTextStyle:
+            getRegularStyle(fontSize: FontSize.s16, color: ColorManager.white)),
 
     // button theme
 
     // Text Theme
 
-
     // input Decoration theme ( text form field)
-
-
-
   );
 }
