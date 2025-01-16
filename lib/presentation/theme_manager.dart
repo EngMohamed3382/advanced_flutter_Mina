098d1a8ffb87439bd06e589271f036764fad6227
Atlas Home <alhefnawimoh@hotmail.com>
2025-01-16 13:51:41 +0200

    part 7 commit - Adding Card theme in theme manager

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,4 +1,5 @@
 import 'package:advanced_flutter/presentation/color_manager.dart';
+import 'package:advanced_flutter/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme(){
@@ -9,11 +10,12 @@ ThemeData getApplicationTheme(){
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager.grey1,
 
-
-
-
     // cardView Theme
-
+    cardTheme:CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    )
 
 
     //app bar theme
