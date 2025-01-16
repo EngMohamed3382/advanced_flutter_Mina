6d507bfde15c6834a5f04351e5ba286840618a6d
Atlas Home <alhefnawimoh@hotmail.com>
2025-01-16 14:06:12 +0200

    part 9 commit - Adding Elevated Button theme in theme manager

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -24,14 +24,33 @@ ThemeData getApplicationTheme() {
     //app bar theme
 
     appBarTheme: AppBarTheme(
-        centerTitle: true,
-        color: ColorManager.primary,
-        elevation: AppSize.s4,
-        shadowColor: ColorManager.lightPrimary,
-        titleTextStyle:
-            getRegularStyle(fontSize: FontSize.s16, color: ColorManager.white)),
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.lightPrimary,
+      titleTextStyle:
+          getRegularStyle(fontSize: FontSize.s16, color: ColorManager.white),
+    ),
 
     // button theme
+
+    buttonTheme: ButtonThemeData(
+      shape: const StadiumBorder(),
+      disabledColor: ColorManager.grey1,
+      buttonColor: ColorManager.primary,
+      splashColor: ColorManager.lightPrimary,
+    ),
+
+    // Elevated Button Theme
+
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+          textStyle: getRegularStyle(
+              color: ColorManager.white, fontSize: FontSize.s17),
+          backgroundColor: ColorManager.primary,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(AppSize.s12))),
+    ),
 
     // Text Theme
 
