3a7f1a630727d1701e7348081b936f0890a5abb1
Atlas Home <alhefnawimoh@hotmail.com>
2025-01-19 12:09:10 +0200

    part 31 commit - MVVM Receive Data in View from ViewModel

--- lib/presentation/onboarding/view/onboarding_view.dart
+++ lib/presentation/onboarding/view/onboarding_view.dart
@@ -1,3 +1,4 @@
+import 'package:advanced_flutter/domain/models.dart';
 import 'package:advanced_flutter/presentation/resources/assets_manager.dart';
 import 'package:advanced_flutter/presentation/resources/color_manager.dart';
 import 'package:advanced_flutter/presentation/resources/constants_manager.dart';
@@ -9,8 +10,6 @@ import 'package:flutter/services.dart';
 import '../../resources/routes_manager.dart';
 import '../viewmodel/onboarding_viewmodel.dart';
 
-
-
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
 
@@ -19,11 +18,10 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnboardingViewState extends State<OnBoardingView> {
-
   final PageController _pageController = PageController();
   final OnBoardingViewModel _viewModel = OnBoardingViewModel();
 
-  _bind(){
+  _bind() {
     _viewModel.start();
   }
 
@@ -33,61 +31,66 @@ class _OnboardingViewState extends State<OnBoardingView> {
     super.initState();
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-    return _getContentWidget();
-  }
-  Widget _getContentWidget(){
-    return Scaffold(
-      backgroundColor: ColorManager.white,
-      appBar: AppBar(
-        backgroundColor: ColorManager.white,
-        elevation: AppSize.s0,
-        systemOverlayStyle: SystemUiOverlayStyle(
-          statusBarColor: ColorManager.white,
-          statusBarBrightness: Brightness.dark,
-        ),
-      ),
-      body: PageView.builder(
-          controller: _pageController,
-          itemCount: _list.length,
-          onPageChanged: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          itemBuilder: (context, index) {
-            return OnBoardingPage(_list[index]);
-          }),
-      bottomSheet: Container(
-        color: ColorManager.white,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, Routes.loginRoute);
-                },
-                child:  Text(
-                  AppStrings.skip,
-                  style: Theme.of(context).textTheme.titleMedium,
-                  textAlign: TextAlign.end,
-                ),
-              ),
-            ),
-            // widgets indicator and arrows
-            _getBottomSheetWidget()
-          ],
-        ),
-      ),
-    );
+    return StreamBuilder<SliderViewObject>(
+        stream: _viewModel.outputSliderViewObject,
+        builder: (context, snapshot) {
+          return _getContentWidget(snapshot.data);
+        });
   }
 
-  Widget _getBottomSheetWidget(){
+  Widget _getContentWidget(SliderViewObject? sliderViewObject) {
+    if (sliderViewObject == null) {
+      return Container();
+    } else {
+      return Scaffold(
+        backgroundColor: ColorManager.white,
+        appBar: AppBar(
+          backgroundColor: ColorManager.white,
+          elevation: AppSize.s0,
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: ColorManager.white,
+            statusBarBrightness: Brightness.dark,
+          ),
+        ),
+        body: PageView.builder(
+            controller: _pageController,
+            itemCount: sliderViewObject.numOfSlides,
+            onPageChanged: (index) {
+              _viewModel.onPageChanged(index);
+            },
+            itemBuilder: (context, index) {
+              return OnBoardingPage(sliderViewObject.sliderObject);
+            }),
+        bottomSheet: Container(
+          color: ColorManager.white,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, Routes.loginRoute);
+                  },
+                  child: Text(
+                    AppStrings.skip,
+                    style: Theme.of(context).textTheme.titleMedium,
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+              ),
+              // widgets indicator and arrows
+              _getBottomSheetWidget(sliderViewObject)
+            ],
+          ),
+        ),
+      );
+    }
+  }
+
+  Widget _getBottomSheetWidget(SliderViewObject sliderViewObject) {
     return Container(
       color: ColorManager.primary,
       child: Row(
@@ -97,29 +100,31 @@ class _OnboardingViewState extends State<OnBoardingView> {
           Padding(
             padding: const EdgeInsets.all(AppPadding.p14),
             child: GestureDetector(
-            child: SizedBox(
-              height: AppSize.s20,
-              width: AppSize.s20,
-              child: SvgPicture.asset(ImageAssets.leftArrowIc),
-            ),
-              onTap: (){
-              // go to Previous Slide
-                _pageController.animateToPage(_getPreviousIndex(), duration: const Duration(milliseconds: AppConstants.sliderAnimationTime), curve: Curves.bounceInOut);
+              child: SizedBox(
+                height: AppSize.s20,
+                width: AppSize.s20,
+                child: SvgPicture.asset(ImageAssets.leftArrowIc),
+              ),
+              onTap: () {
+                // go to Previous Slide
+                _pageController.animateToPage(_viewModel.goPrevious(),
+                    duration: const Duration(
+                        milliseconds: AppConstants.sliderAnimationTime),
+                    curve: Curves.bounceInOut);
               },
+            ),
           ),
+
+          // Circle Indicators
+          Row(
+            children: [
+              for (int i = 0; i < sliderViewObject.numOfSlides; i++)
+                Padding(
+                  padding: const EdgeInsets.all(AppPadding.p8),
+                  child: _getProperCircle(i, sliderViewObject.currentIndex),
+                )
+            ],
           ),
-
-      // Circle Indicators
-        Row(
-          children: [
-            for(int i = 0;  i < _list.length ; i++ )
-              Padding(padding: const EdgeInsets.all(AppPadding.p8),
-              child: _getProperCircle(i),
-              )
-
-          ],
-        ),
-
 
           // Right Arrow
           Padding(
@@ -130,9 +135,12 @@ class _OnboardingViewState extends State<OnBoardingView> {
                 width: AppSize.s20,
                 child: SvgPicture.asset(ImageAssets.rightArrowIc),
               ),
-              onTap: (){
+              onTap: () {
                 // go to Next Slide
-                _pageController.animateToPage(_getNextIndex(), duration: const Duration(milliseconds: AppConstants.sliderAnimationTime), curve: Curves.bounceInOut);
+                _pageController.animateToPage(_viewModel.goNext(),
+                    duration: const Duration(
+                        milliseconds: AppConstants.sliderAnimationTime),
+                    curve: Curves.bounceInOut);
               },
             ),
           ),
@@ -141,15 +149,10 @@ class _OnboardingViewState extends State<OnBoardingView> {
     );
   }
 
-
-
-
-
-  Widget _getProperCircle(int index){
-    if(index == _currentIndex){
+  Widget _getProperCircle(int index, int currentIndex) {
+    if (index == currentIndex) {
       return SvgPicture.asset(ImageAssets.hollowCircleIc);
-    }
-    else{
+    } else {
       return SvgPicture.asset(ImageAssets.solidCircleIc);
     }
   }
@@ -197,4 +200,3 @@ class OnBoardingPage extends StatelessWidget {
     );
   }
 }
-
