b75729a6525ec0a3b2156afb5bf7880cc7f44fdf
Atlas Home <alhefnawimoh@hotmail.com>
2025-01-19 09:30:41 +0200

    part 27 commit - MVVM Adding Stream Controller to Onboarding ViewModel

--- lib/presentation/onboarding/viewmodel/onboarding_viewmodel.dart
+++ lib/presentation/onboarding/viewmodel/onboarding_viewmodel.dart
@@ -1,8 +1,10 @@
+import 'dart:async';
+import 'package:advanced_flutter/domain/models.dart';
 import '../../base/baseviewmodel.dart';
 
 class OnBoardingViewModel extends BaseViewModel implements OnBoardingViewModelInputs, OnBoardingViewModelOutputs{
   // stream controllers outputs
-
+StreamController _streamController = StreamController<SliderViewObject>();
   //OnBoarding ViewModel Inputs
   @override
   void dispose() {
@@ -29,14 +31,28 @@ class OnBoardingViewModel extends BaseViewModel implements OnBoardingViewModelIn
     // TODO: implement onPageChanged
   }
 
+  @override
+  // TODO: implement inputSliderViewObject
+  Sink get inputSliderViewObject => throw UnimplementedError();
+
+
+//OnBoarding ViewModel outputs
+  @override
+  // TODO: implement outPutSliderViewObject
+  Stream get outputSliderViewObject => throw UnimplementedError();
+
 }
 // inputs mean that "Orders" that our view model will receive from view
 abstract class OnBoardingViewModelInputs{
   void goNext();  // when user clicks on right arrow or swipe left
   void goPrevious();  // when user clicks on left arrow or swipe right
   void onPageChanged(int index);
+  // stream controller input
+Sink get inputSliderViewObject;
 }
 
 abstract class OnBoardingViewModelOutputs{
-
+  // stream controller output
+  Stream get outputSliderViewObject;
 }
+
